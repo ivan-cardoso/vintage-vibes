2c937c912855f2d19a4ba040e2e36cd83fde7a19
ivan-cardoso <ivancardoso59@gmail.com>
2021-08-18 14:10:30 -0300

    Deploy build

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import "firebase/firestore";
 const {API_KEY, AUTH_DOMAIN, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID} = process.env
 
 const config = {
-    apiKey: `${API_KEY}`,
-    authDomain: `${AUTH_DOMAIN}`,
-    storageBucket: `${STORAGE_BUCKET}`,
-    messagingSenderId: `${MESSAGING_SENDER_ID}`,
-    appId: `${APP_ID}`,
+    apiKey: "AIzaSyC3458PG75KkaZFbD5u3QDwBkMlxBuJGU0",
+    authDomain: "marketplace-coder.firebaseapp.com",
     projectId: "marketplace-coder",
+    storageBucket: "marketplace-coder.appspot.com",
+    messagingSenderId: "1005800447379",
+    appId: "1:1005800447379:web:432dcdc25a87c705399c66"
 }
 
 const app = firebase.initializeApp(config)
